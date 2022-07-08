f1e0dc273760d0a720ddda74a523554728c37092
D-Pats <devinpatel97@gmail.com>
2022-07-08 22:25:16 +0100

    New SpringBoot project created

---
+++ QA-Hobby-Project/src/main/java/com/qa/hobbyproj/QaHobbyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.qa.hobbyproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaHobbyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaHobbyProjectApplication.class, args);
+	}
+
+}
---
+++ QA-Hobby-Project/src/test/java/com/qa/hobbyproj/QaHobbyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.hobbyproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaHobbyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
